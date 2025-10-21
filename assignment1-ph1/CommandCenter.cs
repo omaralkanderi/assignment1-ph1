c8cef1635dd08ad982308219c9624553f532dab9
Omar AlKanderi <omar.alkander12@gmail.com>
2025-10-21 12:59:41 +0300

    part2 done

---
+++ assignment1-ph1/CommandCenter.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1_ph1
+{
+    internal class CommandCenter
+    {
+        public string userName { get; private set; }
+        public bool isOperational { get; private set; }
+        public int commandsProcessed { get; private set; }
+
+        public CommandCenter()
+        {
+            this.userName = "Guest";
+            this.isOperational = true;
+            this.commandsProcessed = 0;
+        }
+
+        public void run()
+
+        {
+            Console.WriteLine("Welcome to my first project -part2 *_*");
+            Console.WriteLine(); // to make space between lines
+            Console.WriteLine($"Hello {userName} choose the listied command    ");
+            Console.WriteLine("1. help - Display available commands");
+            Console.WriteLine("2. set_name - Set your user name");
+            Console.WriteLine("3. status - Show current status");
+            Console.WriteLine("4. exit - Exit the application");
+            Console.WriteLine();
+            Console.Write("Enter command:  ");
+
+
+            while (isOperational)
+            {
+
+                // prompt the user for input
+                string input = Console.ReadLine();
+                switch (input)
+                {
+
+                    case "help":
+                        ShowHelp();
+
+                        commandsProcessed++;
+                        break;
+                    case "set_name":
+                        userName = setUserNAme(userName);
+                        //Console.WriteLine($"hello { userName}");
+                        repeatcommand(userName);
+                        commandsProcessed++;
+                        break;
+                    case "status":
+                        ShowStatus(userName, isOperational, commandsProcessed);
+                        commandsProcessed++;
+                        repeatcommand(userName);
+                        break;
+                    case "exit":
+                        ExitApplication(userName);
+                        isOperational = false;
+                        break;
+                    default:
+                        InvalidCommand(input);
+                        commandsProcessed++;
+                        //repeatcommand(userName);
+                        break;
+
+
+
+
+
+                }
+
+
+
+
+            }
+        }
+
+        //all methods
+        public static void ShowHelp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Available commands:");
+            Console.WriteLine("help - Display available commands");
+            Console.WriteLine("set_name - Set your user name");
+            Console.WriteLine("status - Show current status");
+            Console.WriteLine("exit - Exit the application");
+            Console.WriteLine();
+            Console.Write("Enter command:  ");
+        }
+        public static string setUserNAme(string name)
+        {
+            Console.WriteLine("Enter your new user name: ");
+            string newName = Console.ReadLine();
+            return newName;
+        }
+        static void ShowStatus(string userName, bool isOperational, int commandsProcessed)
+        {
+            Console.WriteLine("Current Status:");
+            Console.WriteLine($"User Name: {userName}");
+            Console.WriteLine($"Operational: {isOperational}");
+            Console.WriteLine($"Commands Processed: {commandsProcessed}");
+        }
+
+        static void ExitApplication(string userName)
+        {
+            Console.WriteLine($"Goodbye, {userName}.");
+
+        }
+        static void InvalidCommand(string command)
+        {
+            Console.WriteLine($"Invalid command: '{command}'. Type 'help' to see available commands.");
+        }
+        static void repeatcommand(string userName)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Hello {userName} choose the listied command    ");
+            Console.WriteLine("1. help - Display available commands");
+            Console.WriteLine("2. set_name - Set your user name");
+            Console.WriteLine("3. status - Show current status");
+            Console.WriteLine("4. exit - Exit the application");
+            Console.WriteLine();
+            Console.Write("Enter command:  ");
+        }
+    }
+
+}
