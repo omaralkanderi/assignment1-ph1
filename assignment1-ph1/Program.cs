a47247b1a265eab5a318dc9cbb039abd475575cc
Omar AlKanderi <omar.alkander12@gmail.com>
2025-10-28 23:50:44 +0300

    part4 done

--- assignment1-ph1/Program.cs
+++ assignment1-ph1/Program.cs
@@ -11,7 +11,7 @@ namespace assignment1_ph1
         static void Main(string[] args)
         {
             CommandCenter commandCenter = new CommandCenter();
-            commandCenter.run();
+            commandCenter.RunAsync();
     
         }
         }
